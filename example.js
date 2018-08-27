86ea84c4a5b5f638dc602957a9e1e28615f7041b
lependu <lependu@protonmail.com>
2018-08-27 15:08:48 +0200

    Add next asset routes to example

--- example.js
+++ example.js
@@ -5,7 +5,16 @@ const fastify = require('fastify')()
 fastify
   .register(require('./index'))
   .after(() => {
+    // Add this to serve the _next assets
+    fastify.next('/_next/*', (app, req, reply) => {
+      return app.handleRequest(req.req, reply.res)
+        .then(() => {
+          reply.sent = true
+        })
+    })
+    // Then register your routes
     fastify.next('/hello')
+
   })
 
 fastify.listen(3000, err => {
