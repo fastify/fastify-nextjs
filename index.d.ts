f36f8001129f5ee1f57645050821ff2d82894a8c
ddot <cnljli@live.com>
2019-03-07 10:56:30 +0800

    add fastify next decorators

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,40 @@
-import { Plugin } from "fastify";
-import { Server, IncomingMessage, ServerResponse } from "http"
+
+import {
+  FastifyReply,
+  FastifyRequest,
+  HTTPMethod,
+  Plugin,
+  RouteSchema,
+} from 'fastify';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import { DevServer } from 'next';
+import { UrlLike } from 'next/router';
+
+declare module 'fastify' {
+  type FastifyNextCallback = (
+    app: DevServer,
+    req: FastifyRequest<any>,
+    reply: FastifyReply<any>
+  ) => Promise<void>;
+
+  interface FastifyInstance<
+    HttpServer = Server,
+    HttpRequest = IncomingMessage,
+    HttpResponse = ServerResponse
+  > {
+    next(
+      path: string,
+      opts:
+        | {
+            method: HTTPMethod;
+            schema: RouteSchema;
+            next: UrlLike;
+          }
+        | FastifyNextCallback,
+      handle?: FastifyNextCallback
+    ): void;
+  }
+}
 
 declare const fastifyReact: Plugin<
   Server,
