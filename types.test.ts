d3f73c2d46e0d70851c8ab7c116788bb9e2c86c9
Jetsly <cnljli@live.com>
2019-03-08 15:33:28 +0800

    add next method test and next-types

--- types.test.ts
+++ types.test.ts
@@ -3,4 +3,10 @@ import fastifyReact = require('./index');
 
 const app = fastify();
 
-app.register(fastifyReact).after(() => {});
+app.register(fastifyReact).after(() => {
+  app.next('/*', (nextApp, req, reply) => {
+    return nextApp.handleRequest(req.req, reply.res).then(() => {
+      reply.sent = true;
+    });
+  });
+});
