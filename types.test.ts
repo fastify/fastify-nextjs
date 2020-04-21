755992520b390c0af9519b9105d6644f3bdb2053
Jule- <Jule-@users.noreply.github.com>
2020-04-21 16:16:23 +0200

    feat(next): Update to next 9.3.1 (#83)
    * feat(next): Update to next 9.3.1
    
    Integrate Next.js breaking changes
    
    * chore(gitignore): Revert changes for package-lock.json in .gitignore
    
    * chore(gitignore): Actually remove package-lock.json for real...
    
    * ci(node): Update node supported versions
    
    Following Next.js requirements, dropping node 8 support.
    See https://nextjs.org/docs#system-requirements
    Add support to node 12 and 13.
    
    BREAKING CHANGE: Node 8 is no longer supported

--- types.test.ts
+++ types.test.ts
@@ -1,12 +1,14 @@
-import fastify = require('fastify');
-import fastifyReact = require('./index');
+import fastify from 'fastify';
+import fastifyReact from './index';
 
 const app = fastify();
 
 app.register(fastifyReact).after(() => {
   app.next('/*', (nextApp, req, reply) => {
-    return nextApp.handleRequest(req.req, reply.res).then(() => {
-      reply.sent = true;
-    });
+    return nextApp
+      .getRequestHandler()(req.req, reply.res)
+      .then(() => {
+        reply.sent = true;
+      });
   });
 });
