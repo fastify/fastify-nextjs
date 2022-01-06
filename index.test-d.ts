be0b39509c62b54b5cd134a14146cf833e3fbc1f
Francois Best <francois@francoisbest.com>
2022-01-06 22:56:32 +0100

    feat(types): Allow shorthand route options in `.next`
    With the addition of an optional `method` to use.
    
    Add test demoing use of route-local `onRequest` hook
    for custom property injection.
    
    Fixes #432.

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,12 @@
 import fastify, { FastifyInstance } from 'fastify';
-import { expectError, expectType } from 'tsd';
 import fastifyNext from './index';
+
+// Declaration merging for custom property injection
+declare module 'http' {
+  interface IncomingMessage {
+    server: FastifyInstance
+  }
+}
 
 const app = fastify();
 
@@ -45,4 +51,11 @@ app
         reply.send('OK');
       }
     );
+
+    app.next('/options-with-hook-injecting-custom-property', {
+      onRequest: (req, _, done) => {
+        req.raw.server = req.server;
+        done();
+      },
+    });
   });
